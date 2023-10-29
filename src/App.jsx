e42323298315d982bbbcfc7685b758e5012a6795
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-29 17:49:51 +0600

    Link-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
-import DuNaver from './assets/Compunents/DaisyUI/DuNaver'
+import Nav from './assets/Compunents/Navber/Nav'
+// import DuNaver from './assets/Compunents/DaisyUI/DuNaver'
 
 function App() {
 
 
   return (
     <>
-       <DuNaver></DuNaver>
+       {/* <DuNaver></DuNaver> */}
+       <Nav></Nav>
       <h1 className='text-4xl font-bold bg-green-500'>Vite + React</h1>
  
     </>
